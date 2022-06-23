ecbd30426ab235cc7441890659ec9fcd6728ea98
Luiz Guilherme Nunes dos Santos <15866421623@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-23 09:08:34 -0300

    Projeto Inicial

---
+++ controller/Categoria.php
@@ -0,0 +1,10 @@
+<?php
+require "../model/CategoriaModel.php";
+
+$categoria = new CategoriaModel();
+$categoria->inserir("Produtos Limpeza");
+$categoria->excluir(1);
+$categoria->atualizar(2, "SmartPhone");
+var_dump($categoria->buscarPorId(3));
+var_dump($categoria->buscarTudo());
+?>
