bc8286ea064ce72a2de67728676140fc837c0dc5
Luiz Guilherme Nunes dos Santos <15866421623@dc01.ifs.ifsuldeminas.edu.br>
2022-06-29 16:56:13 -0300

    front-controller

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -1,10 +1,25 @@
 <?php
 require "../model/CategoriaModel.php";
 
-$categoria = new CategoriaModel();
-$categoria->inserir("Produtos Limpeza");
-$categoria->excluir(1);
-$categoria->atualizar(2, "SmartPhone");
+class Categoria{
+
+    function __construct (){
+    $this->modelo= new CategoriaModel();
+}
+function index(){
+    include "view/template/conteudo.php";
+}
+function add(){
+    echo "mostrar from categoria";
+}
+function excluir($id){
+    echo "excluir categoria";
+}
+}
+// categoria = new CategoriaModel();
+// categoria->inserir("Produtos Limpeza");
+// categoria->excluir(1);
+// categoria->atualizar(2, "SmartPhone");
 var_dump($categoria->buscarPorId(3));
 var_dump($categoria->buscarTudo());
 ?>
